282260be239e9942292186b9235504d3b7ec4a00
Jyotirmoy Halder <jyotirmoy.cse5.bu@gmail.com>
2025-11-20 16:42:25 +0600

    Initial Commit

---
+++ GratefulMoments/GratefulMomentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GratefulMomentsApp.swift
+//  GratefulMoments
+//
+//  Created by jyotirmoy_halder on 20/11/25.
+//
+
+import SwiftUI
+
+@main
+struct GratefulMomentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
